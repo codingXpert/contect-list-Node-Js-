9bb0b5c924bc0eb943be9643a70b19acbd504781
Vivek <vivekraj78965.vr@gmail.com>
2022-08-28 07:58:03 +0530

    setting up view engine

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 const port = 8000;
 
 const app = express();
 
+app.set("view engine" , "ejs") //app.set sets the value for the key as (view engine:ejs)
+app.set('views' , path.join (__dirname , 'views'));
 
 
-
-
+app.get('/' , function(req , res){
+  return  res.render('home'); 
+})
 
 
 
