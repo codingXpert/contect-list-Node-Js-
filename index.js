13f5f8533d6c190f1cbd118b1740470dd4158aba
Vivek <vivekraj78965.vr@gmail.com>
2022-08-28 09:05:40 +0530

    Creating A Contact List

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const port = 8000;
 
 const app = express();
 
-app.set("view engine" , "ejs") //app.set sets the value for the key as (view engine:ejs)
-app.set('views' , path.join (__dirname , 'views'));
+app.set("view engine", "ejs"); //app.set sets the value for the key as (view engine:ejs)
+app.set("views", path.join(__dirname, "views"));
 
 
-app.get('/' , function(req , res){
-  return  res.render('home' , {title:'My Contact List'}); 
-})
+//creating contacts list
+let contactList = [
+  {
+    name: "Dhanush Kr.",
+    phone: "1111111111",
+  },
+  {
+    name: "Mohan Kr.",
+    phone: "1234567890",
+  },
+  {
+    name: "Rohan Kr.",
+    phone: "0987654321",
+  },
+];
+
+
+//routes
+app.get("/", function (req, res) {
+  return res.render("home", {
+    title: "Contact List",
+    contact_list: contactList,
+  });
+});
 
 
 
+//creating the server
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error", err);
+  }
 
-
-
-app.listen(port , function(err){
-    if(err){
-        console.log("Error" , err);
-    }
-
-    console.log('Server is running on the port::' , port)
-})
+  console.log("Server is running on the port::", port);
+});
