57401562e0ec53ba2df7d5ca2f7ead885bc45ecc
Vivek <vivekraj78965.vr@gmail.com>
2022-08-28 07:07:04 +0530

    creating server using express

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port , function(err){
+    if(err){
+        console.log("Error" , err);
+    }
+
+    console.log('Server is running on the port::' , port)
+})
