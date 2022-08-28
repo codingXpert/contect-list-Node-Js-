da951885bcf98fa94f5c2ec764def614884cda40
Vivek <vivekraj78965.vr@gmail.com>
2022-08-28 08:12:44 +0530

    setting dynamic title using ejs syntax

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('views' , path.join (__dirname , 'views'));
 
 
 app.get('/' , function(req , res){
-  return  res.render('home'); 
+  return  res.render('home' , {title:'My Contact List'}); 
 })
 
 
